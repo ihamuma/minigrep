ec2eac7277bb954da19f5ea4328f4a59a6863124
ihamuma <markus.ihamuotila@gmail.com>
2025-02-08 23:36:49 +0200

    Clippy stuff

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
+
     let query = &args[1];
     let file_path;
     file_path = &args[2];
 
     println!("Searching for {query}");
     println!("In file {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Couldn't read file");
 }
