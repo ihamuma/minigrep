a68b92a005a78406c9d92afa744342ecb5694c7b
ihamuma <markus.ihamuotila@gmail.com>
2025-02-09 09:36:50 +0200

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Encountered error while parsing arguments:\n {err}");
+        process::exit(1);
+    });
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Couldn't read file");
+    run(config);
+}
 
-    println!("With text:\n{contents}")
+fn run(config: Config) {
+    let contents = fs::read_to_string(config.file_path).expect("Couldn't read file");
+
+    println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].to_string();
+        let file_path = args[2].to_string();
+
+        Ok(Config { query, file_path })
+    }
 }
